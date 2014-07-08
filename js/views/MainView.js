7158bcb6d74d8c9df808c46572ae22f1bd7b4afa
Marcus Christie <marc.christie@gmail.com>
2014-07-08 19:46:05 -0400

    Added destination search text input

--- js/views/MainView.js
+++ js/views/MainView.js
@@ -1,10 +1,18 @@
 
-define(['backbone', 'jquery', 'gmaps', 'views/PageView', 'text!templates/main.html'], function(Backbone, $, gmaps, PageView, mainTemplate) {
+define(['backbone', 'jquery', 'gmaps', 'views/PageView', 'text!templates/main.html'],
+function(Backbone, $, gmaps, PageView, mainTemplate) {
+    'use strict';
+
+    var ENTER_KEY = 13;
     var MainView = PageView.extend({
 
         id: 'main-view',
 
         template:_.template(mainTemplate),
+
+        events: {
+            'keypress #destination': 'loadOnEnter'
+        },
 
         render:function (eventName) {
             $(this.el).html(this.template());
@@ -13,10 +21,22 @@ define(['backbone', 'jquery', 'gmaps', 'views/PageView', 'text!templates/main.ht
               center: new gmaps.LatLng(-34.397, 150.644),
               mapTypeId: gmaps.MapTypeId.ROADMAP
             };
-            var map = new gmaps.Map(this.$el.find('#map-canvas').get(0), myOptions);
+            var map = new gmaps.Map(this.$('#map-canvas').get(0), myOptions);
             this.enhance();
             return this;
+        },
+
+        loadOnEnter: function( event ){
+            var destination = this.$("#destination");
+            if (event.which !== ENTER_KEY || !destination.val().trim() === "" ) {
+                return;
+            }
+
+            console.log("We're going to " + destination.val());
+            // TODO: load directions
+            // TODO: load gas prices
         }
+
     });
 
     return MainView;
